aa0eb337168761bec82a55aeb4c6b11bff693975
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-18 12:08:09 -0800

    Initial Commit

---
+++ moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by ahmad kaddoura on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
